3904ec80518499d6e5db6c137f34f22dea4969cb
skywater98 <skywater98@naver.com>
2025-12-18 15:20:32 +0900

    Add linked list

---
+++ data_structures/linked_list.py
@@ -0,0 +1,18 @@
+class Node:
+    def __init__(self, data):
+        self.data = data
+        self.next = None
+
+
+class LinkedList:
+    def __init__(self):
+        self.head = None
+
+    def append(self, data):
+        if not self.head:
+            self.head = Node(data)
+            return
+        curr = self.head
+        while curr.next:
+            curr = curr.next
+        curr.next = Node(data)
