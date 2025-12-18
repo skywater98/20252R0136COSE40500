37fd1e28508005caaef567d829d1b05962ed70ad
skywater98 <skywater98@naver.com>
2025-12-18 15:17:56 +0900

    Add queue

---
+++ data_structures/queue.py
@@ -0,0 +1,12 @@
+class Queue:
+    def __init__(self):
+        self.items = []
+
+    def enqueue(self, item):
+        self.items.append(item)
+
+    def dequeue(self):
+        return self.items.pop(0) if not self.is_empty() else None
+
+    def is_empty(self):
+        return len(self.items) == 0
