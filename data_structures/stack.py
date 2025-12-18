3ee9cfe20816a20b9e1fd63e6a3c31fcc5db79d6
skywater98 <skywater98@naver.com>
2025-12-18 15:16:41 +0900

    Add stack

---
+++ data_structures/stack.py
@@ -0,0 +1,12 @@
+class Stack:
+    def __init__(self):
+        self.items = []
+
+    def push(self, item):
+        self.items.append(item)
+
+    def pop(self):
+        return self.items.pop() if not self.is_empty() else None
+
+    def is_empty(self):
+        return len(self.items) == 0
